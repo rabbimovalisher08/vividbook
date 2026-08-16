3afeb91e4a4088edd1f06d5c06b35d1608fd8257
rabbimovalisher08 <rabbimovalisher065@gmail.com>
2026-08-16 09:14:20 +0500

    Day 12 prompt welcome

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function showMessage(topic) {
     alert("Welcome to " + topic + " lesson!");
 }
+function welcomeUser() {
+    let name = prompt("What is your name?");
+
+    if (name) {
+        alert("Welcome to VividBook, " + name + "!");
+    } else {
+        alert("Welcome to VividBook!");
+    }
+}
